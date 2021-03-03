91729ab455c491608457c1a6f6b5df8f3235d565
KVlearn <kavya.mdu@gmail.com>
2021-03-02 19:30:53 -0800

    done

--- data/seeds/01-clean.js
+++ data/seeds/01-clean.js
@@ -3,6 +3,6 @@ exports.seed = async function(knex) {
     //remember to those tables with dependencies first!
     await knex("ingredients").truncate()
     await knex("instructions").truncate()
-    await knex("recipes").truncate()
     await knex("users").truncate()
+    await knex("recipes").truncate()
 };
