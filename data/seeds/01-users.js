012045285e41c9ea5f463e93fee44baabe2a52da
KVlearn <kavya.mdu@gmail.com>
2021-03-02 20:54:28 -0800

    modified migration db

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -2,7 +2,7 @@ const bcryptjs = require("bcryptjs");
 
 exports.seed = async function(knex) {
   await knex("users").insert([
-    {username:"test",password: bcryptjs.hashSync("password",8)},
-    {username:"helloz",password: "$2a$08$v0xBLPNERfCAVVJl5.I2l.BqxjN7B4tZjs710UVQzs9Z11hDAKiAm"},
+    {username:"testworld",password: bcryptjs.hashSync("password",8)},
+    {username:"hellozworld",password: "$2a$08$v0xBLPNERfCAVVJl5.I2l.BqxjN7B4tZjs710UVQzs9Z11hDAKiAm"},
    ])
 };
