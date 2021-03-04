04ca0f33ba2be7c4098d42f5784d9e0da01aa26d
KVlearn <kavya.mdu@gmail.com>
2021-03-03 17:26:02 -0800

    pg modified

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require("knex");
 //creating instance of knex - to configure db from the knexfile setup
 const config = require("../knexfile.js");
 
-const environment = process.env.DB_ENV || "development";
+const environment = process.env.DB_ENV || 'development';
 
 module.exports = knex(config[environment]);
--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,8 @@
 // Update with your config settings.
 require("dotenv").config()
-const pgConnection = process.env.DATABASE_URL;
+// const pgConnection = process.env.DATABASE_URL;
+
+const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";// if using a local postgres server, please create the database manually, Knex will not create it autmatically
 
 module.exports = {
   development: {
